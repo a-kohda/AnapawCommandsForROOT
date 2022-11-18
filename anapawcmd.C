6c4dbe50ec71760faa10a2205561b50e0022df3b
A. Kohda <kohda@example.com>
2022-11-18 18:57:25 +0900

    Update

--- anapawcmd.C
+++ anapawcmd.C
@@ -371,3 +371,290 @@ TList* GetHistList(){
 	return li;
 }
 
+
+// saho版からのコピー
+float figali(float xmin, float xmax, 
+		bool oldel =true, bool print = true, int kreturn =0, 
+		TH1* ihist = 0x0 ){
+	// oldel: old line delete
+	// ihist 対象とするHist, 未入力or 0x0 指定で現在のヒストを取得する。
+	
+	TH1* h1;
+	bool arghist = false; // 引数で処理するhistを指定してるか
+	if(ihist == 0x0){
+		h1 = ht();
+	}else{
+		h1 = ihist;
+		//ihist->Draw();
+		arghist = true;
+	}
+
+	// 以前この関数を使った際に描いたTGraphを消す
+/*	if(gPad->GetListOfPrimitives()->FindObject("fli") != nullptr){
+		gPad->GetListOfPrimitives()->FindObject("fli")->Delete();
+	}
+	if(gPad->GetListOfPrimitives()->FindObject("fppl0") != nullptr){
+		gPad->GetListOfPrimitives()->FindObject("fppl0")->Delete();
+	}
+	if(gPad->GetListOfPrimitives()->FindObject("fppl1") != nullptr){
+		gPad->GetListOfPrimitives()->FindObject("fppl1")->Delete();
+	}
+	if(gPad->GetListOfPrimitives()->FindObject("fppl2") != nullptr){
+		gPad->GetListOfPrimitives()->FindObject("fppl2")->Delete();
+	}
+*/
+
+	float initialmin, initialmax;
+	
+	initialmin = h1->GetXaxis()->GetBinCenter(h1->GetXaxis()->GetFirst());
+	initialmax = h1->GetXaxis()->GetBinCenter(h1->GetXaxis()->GetLast());
+	h1->GetXaxis()->SetRangeUser(xmin,xmax);
+	
+
+	TF1 *fit1 = new TF1("fit1","[0]*exp(-0.5*((x-[1])/[2])*((x-[1])/[2]))+[3]*x+[4]",xmin ,xmax);
+	fit1->SetParameter(0,h1->GetBinContent(h1->GetMaximumBin()));
+	fit1->SetParameter(1,h1->GetBinCenter(h1->GetMaximumBin()));
+	fit1->SetParameter(2,(xmax - xmin)/10 );
+
+	float tempx1 = xmin;
+	float tempy1 = h1->GetBinContent(h1->FindBin(xmin));
+	float tempx2 = xmax;
+	float tempy2 = h1->GetBinContent(h1->FindBin(xmax));
+	float tempa  = (tempy2-tempy1)/(tempx2-tempx1);
+	float tempb  = tempy1 - tempa * tempx1;
+	fit1->SetParameter(3,tempa);
+	fit1->SetParameter(4,tempb);
+
+	h1->Fit("fit1","RQ","");
+
+	TGraph* gfit1 = new TGraph(fit1);
+	gfit1->SetLineColor(2);
+	gfit1->SetLineWidth(2);
+	gfit1->Draw("same");
+
+	TF1* fli = new TF1("fli",Form("%f*x+%f",fit1->GetParameter(3),fit1->GetParameter(4)),xmin ,xmax);
+
+	TGraph* gli = new TGraph(fli);
+	gli->SetLineColor(4);
+	gli->SetLineWidth(1);
+	if(!arghist){
+		gli->Draw();
+	}else{
+		gli->Draw("same");
+	}
+
+	TGraph* ppl[3];
+	float pplx[3];
+	pplx[0] = fit1->GetParameter(1); // Mean
+	pplx[1] = fit1->GetParameter(1) - 5 * TMath::Abs(fit1->GetParameter(2)); // Mean - 5 sigma
+	pplx[2] = fit1->GetParameter(1) + 5 * TMath::Abs(fit1->GetParameter(2)); // Mean + 5 sigma
+	for (int k=0; k<3; k++) {
+		ppl[k] = new TGraph();
+		ppl[k]->SetName(Form("fppl%d",k));
+		ppl[k]->SetPoint(0, pplx[k], 0);
+		ppl[k]->SetPoint(1, pplx[k], INFINITY);
+		ppl[k]->SetLineColor(3);
+		if(!arghist){ ppl[k]->Draw(); } else { ppl[k]->Draw("same"); }
+	}
+	
+
+	int startbin = h1->FindBin(pplx[1]);
+	int endbin   = h1->FindBin(pplx[2]);		
+	float startval = h1->GetBinLowEdge(startbin);
+	float endval   = h1->GetBinLowEdge(endbin+1);
+
+//	printf("Constant : %f\n", fit1->GetParameter(0));
+
+	float integral      = h1->Integral(startbin,endbin);
+	float integralerr   = TMath::Sqrt(integral);
+	float background    = fli->Integral(startval,endval)/0.25; // 要改善
+	float backgrounderr = TMath::Sqrt(background);
+	float calcedpeak    = integral - background;
+	float calcedpeakerr = TMath::Sqrt(integral + background);
+
+	if(print){
+		printf("\n");
+		printf("Center   : %#8g\n", fit1->GetParameter(1));
+		printf("Sigma    : %#8g\n", TMath::Abs(fit1->GetParameter(2)));
+		printf("Integral : %#8g  +-  %#8g\n", integral, integralerr );
+		printf("CalcInte : %#8g\n", fit1->Integral(startval,endval)/0.25); // 要改善
+		printf("B.G.     : %#8g  +-  %#8g\n", background, backgrounderr); 
+		printf("Int - BG : %#8g  +-  %#8g\n", calcedpeak, calcedpeakerr); 
+
+		printf("for me   : %#8g\t%#8g\t%#8g\n",fit1->GetParameter(1), calcedpeak, calcedpeakerr ); 
+		printf("\n");
+	}
+
+	//xrange(initialmin, initialmax);
+	if(!arghist){
+		h1->GetXaxis()->SetRangeUser(initialmin, initialmax);
+		h1->Draw("hist same");
+	}
+//gli->Draw();
+
+	return fit1->GetParameter(1);
+}
+
+void xfigali(){
+	printf("Click a Start and End point of fit range!\n");
+	xgetvalx(2);
+	figali(globalxvaldata.x[0], globalxvaldata.x[1]);
+}
+
+// 関数記述部
+
+TH1* ht(int n, TString opt){ // n番目のhistをDraw (オプション指定)
+	TH1* h1 = (TH1*)GetHistList()->At(n);
+	h1->SetOption(opt);
+	h1->Draw();
+	return h1;
+}
+
+TH1* ht(int n){ // n番目のhistをDraw (オプション維持)
+	TList* li = GetHistList();
+	TH1* h1 = (TH1*)li->At(n);
+	//li->Delete();
+	h1->Draw();
+	return h1;
+}
+
+TH1* ht(){ // 現在表示されているhistのポインタを返すだけ ( ht()->Draw()的な使い方が出来るように )
+	TH1F *h1 = (TH1F*)gPad->GetListOfPrimitives()->At(1);
+	return h1;
+}
+
+TH1* GetCurrentHist(){
+	TH1* h1;
+	if( gPad == 0x0 ){
+		h1 = 0x0;
+		fprintf(stderr, "Warning! No current TH1 !\n");
+	}else{
+		h1 = (TH1*)gPad->GetListOfPrimitives()->At(1);
+	}
+	return h1;
+}
+
+TH1* htp(int n=-1){ // Drawせずにポインタだけを返す
+	TH1* h1;
+	if(n<0){
+		h1 = GetCurrentHist();
+	}else{
+		TList* li = GetHistList();
+		h1 = (TH1*)li->At(n);
+	}
+	return h1;
+}
+
+
+TH1* ht(TString opt){ // 現在表示されているhistのoptionを変更して再Draw
+	TH1F *h1 = (TH1F*)gPad->GetListOfPrimitives()->At(1);
+	h1->SetOption(opt);
+	h1->Draw();
+	return h1;
+}
+
+
+TH1* hn(TString opt){ // 現在表示されているhistの次のhistを表示
+	TList* li = GetHistList();
+	TH1* h1 = (TH1*)li->After(gPad->GetListOfPrimitives()->At(1));
+	h1->SetOption(opt);
+	h1->Draw();
+	printf(" Draw ID:%3d  %s\n",li->IndexOf(h1),h1->GetName());
+	return h1;
+}
+
+TH1* hn(){ // ↑のオプション維持版
+	TList* li = GetHistList();
+	TH1* h1 = (TH1*)li->After(gPad->GetListOfPrimitives()->At(1));
+	h1->Draw();
+	printf(" Draw ID:%3d  %s\n",li->IndexOf(h1),h1->GetName());
+	return h1;
+}
+
+TH1* hb(TString opt){ // 現在表示されているhistの前のhistを表示
+	TH1* h1 = (TH1*)GetHistList()->Before(gPad->GetListOfPrimitives()->At(1));
+	h1->SetOption(opt);
+	h1->Draw();
+	return h1;
+}
+
+TH1* hb(){ // ↑のオプション維持版
+	TH1* h1 = (TH1*)GetHistList()->Before(gPad->GetListOfPrimitives()->At(1));
+	h1->Draw();
+	return h1;
+}
+
+void figa(){ // 現在表示されているhistをGausianでfit (xrangeで範囲調整してからのほうが良い)
+	TH1F *h1 = (TH1F*)gPad->GetListOfPrimitives()->At(1);
+	h1->Fit("gaus");
+}
+
+void xrange(float xmin, float xmax){ // (オリジナル) 現在表示されているhistのx軸の表示範囲を設定
+//	TObject *obj = gDirectory->GetListOfKeys()->FindObject(gPad->GetListOfPrimitives()->At(1)->GetName());
+//	TH1 *h1 = (TH1*)gDirectory->Get(obj->GetName());
+	TH1F *h1 = (TH1F*)gPad->GetListOfPrimitives()->At(1);
+	h1->GetXaxis()->SetRangeUser(xmin,xmax);
+	h1->Draw("hist same");
+}
+
+void xrange(){ // (オリジナル) 現在表示されているhistのx軸の表示範囲をデフォルトに戻す
+//	TObject *obj = gDirectory->GetListOfKeys()->FindObject(gPad->GetListOfPrimitives()->At(1)->GetName());
+//	TH1 *h1 = (TH1*)gDirectory->Get(obj->GetName());
+	TH1F *h1 = (TH1F*)gPad->GetListOfPrimitives()->At(1);
+	h1->GetXaxis()->UnZoom();
+	h1->Draw("same");
+}
+
+void lny(){ // y軸をリニアスケールにする
+	gPad->SetLogy(0);
+}
+
+void lgy(){ // y軸をログスケールにする
+	gPad->SetLogy(1);
+}
+
+void hlist(){
+	//int n=5;
+	printf("\n  ===> Histogram List\n\n");
+	printf("    HID    Kind    Name\n\n");
+
+	TList* li = GetHistList();
+	int cidx = -1;
+	TString arrow;
+	if(gPad != nullptr){
+		cidx = li->IndexOf(gPad->GetListOfPrimitives()->At(1));
+	}
+	//printf("Current hist index is : %d\n",cidx);
+
+	for(int n=0;n < li->GetEntries();n++){
+		int kind=9;
+		TObject* obj = li->At(n);
+		if(obj->InheritsFrom("TH3")) { kind = 3; }
+		else if(obj->InheritsFrom("TH2")) { kind = 2; }
+		else if(obj->InheritsFrom("TH1")) { kind = 1; }
+	
+		if ( n == cidx ) { arrow = "->"; }
+		else { arrow = "  "; }
+		printf(" %s %3d    (%d)    %s\n", arrow.Data(), n, kind, obj->GetName());
+	}
+	//li->Delete();
+}
+
+
+
+TList* GetHistList(){
+	TList *li = new TList();
+	if(gDirectory->GetListOfKeys() != nullptr){
+		for(int i=0; i< gDirectory->GetListOfKeys()->GetEntries(); i++){
+			li->Add(gROOT->FindObject(gDirectory->GetListOfKeys()->At(i)->GetName()));
+		}
+	}
+
+	for(int i=0; i< gROOT->GetList()->GetEntries(); i++){
+		li->Add(gROOT->GetList()->At(i));
+	}
+	return li;
+}
+
+
+
