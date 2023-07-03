157ea1b548136d3419d60f8a2c016eff4bd2814c
Asahi Kohda <kohda@rcnp.osaka-u.ac.jp>
2023-07-03 19:16:35 +0900

    Update

--- docs/menudata.js
+++ docs/menudata.js
@@ -24,7 +24,6 @@
 */
 var menudata={children:[
 {text:"総合概要",url:"index.html"},
-{text:"諸情報",url:"pages.html"},
 {text:"ファイル",url:"files.html",children:[
 {text:"ファイル一覧",url:"files.html"},
 {text:"ファイルメンバ",url:"globals.html",children:[
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['anapaw_20commands_20for_20root_0',['ANAPAW Commands for ROOT',['../md_ReadMe.html',1,'']]]
+  ['anapaw_20commands_20for_20root_0',['ANAPAW Commands for ROOT',['../index.html',1,'']]]
 ];
